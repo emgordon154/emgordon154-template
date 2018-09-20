caf0dcc67b15de70698765a3afab53f10f78961f
emgordon154 <emgordon154@gmail.com>
2018-09-19 23:19:29 -0400

    i think this is getting to be a pretty good template

--- app/main.jsx
+++ app/main.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 import {render} from 'react-dom'
 
+const placeholder = <div id="placeholder" > Placeholder </div>
+
 render(
-  <div id="placeholder" > Placeholder </div>,
+  placeholder,
   document.getElementById('main')
 )
